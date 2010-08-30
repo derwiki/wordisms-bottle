9d5a429c7ad1eb2fd92f118087c58c1ef36d2bc1
Adam Derewecki <derewecki@gmail.com>
2010-08-30 09:33:06 -0700

    why won't the datastore work

--- gae-main.py
+++ gae-main.py
@@ -1,27 +1,27 @@
+import time
+
 import bottle
 from bottle import route, run
 from google.appengine.ext.webapp import util 
 from google.appengine.ext import db
 
-import time
+from models import Response
+
+
 bottle.debug(True)
 
 @route('/')
 def index():
-	context = dict(time_rendered=time.time())
 	try:
-		class Response(db.Expando):
-			time = db.DateTimeProperty(auto_now_add=True)
-
+		context = dict(time_rendered=time.time(), name='Adam Derewecki')
 		resp = Response(**context)
 		resp.put()
+		context['models'] = Response.all().fetch(50)
+		context['gql'] = Response.gql('LIMIT 10')
+		return str(context)
 	except Exception, e:
-		return e
-
-	context['models'] = Response.all()
-	context['resp'] = resp.all()
-	return str(context)
-	return "Byam, son! %(time_rendered)s\n\nmodels: %(models)s(end)" % context
+		import traceback
+		return traceback.format_exc()
 
 @route('/crash')
 def crash():
